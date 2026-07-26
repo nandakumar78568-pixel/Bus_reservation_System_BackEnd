fc17a3f64cefeac61b2fb3122a6ce65f58173195
Nanda Kumar <nandakumar78568@gmail.com>
2026-07-26 21:14:08 +0530

    buses

--- src/main/java/com/busreservation/controller/RoutePointsController.java
+++ src/main/java/com/busreservation/controller/RoutePointsController.java
@@ -2,9 +2,10 @@ package com.busreservation.controller;
 
 import com.busreservation.model.BoardingPoint;
 import com.busreservation.repository.BoardingPointRepository;
+import com.busreservation.repository.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/routes")
@@ -13,8 +14,19 @@ public class RoutePointsController {
     @Autowired
     private BoardingPointRepository boardingPointRepository;
 
+    @Autowired
+    private RouteRepository routeRepository;
+
     @GetMapping("/{routeId}/points")
     public List<BoardingPoint> getPoints(@PathVariable Integer routeId) {
         return boardingPointRepository.findByRoute_RouteId(routeId);
+    }
+
+    @GetMapping("/cities")
+    public List<String> getCities() {
+        Set<String> cities = new TreeSet<>(); // sorted, no duplicates
+        cities.addAll(routeRepository.findDistinctSources());
+        cities.addAll(routeRepository.findDistinctDestinations());
+        return new ArrayList<>(cities);
     }
 }

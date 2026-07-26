77b5ff0cd932971ffe4413231f93a39d10b34ad2
Nanda Kumar <nandakumar78568@gmail.com>
2026-07-26 20:49:47 +0530

    route

--- src/main/java/com/busreservation/repository/RouteRepository.java
+++ src/main/java/com/busreservation/repository/RouteRepository.java
@@ -2,5 +2,14 @@ package com.busreservation.repository;
 
 import com.busreservation.model.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface RouteRepository extends JpaRepository<Route, Integer> {}
+public interface RouteRepository extends JpaRepository<Route, Integer> {
+
+    @Query("SELECT DISTINCT r.source FROM Route r")
+    List<String> findDistinctSources();
+
+    @Query("SELECT DISTINCT r.destination FROM Route r")
+    List<String> findDistinctDestinations();
+}
